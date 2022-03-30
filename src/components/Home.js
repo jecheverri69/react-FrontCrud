dcaeaec117d7eeb0c60065493f0b5b3013167561
jerson <jerson.echeverri@lumni.net>
2022-03-29 22:57:32 -0500

    entrega final

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,15 @@
-import React from "react";
 import { makeStyles } from "@mui/styles";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 import { useState } from "react";
 import { DialogContent, Grid } from "@mui/material";
 import { ThemeProvider } from "@mui/material";
 import theme from "./style/theme";
 import MUIDataTable from "mui-datatables";
 import { useEffect } from "react";
-import {
-	getUsers,
-	AddUser,
-	DeleteUser,
-	UpdateUser,
-} from "../Api/actions/users";
 import Dialog from "@mui/material/Dialog";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
 
 const useStyles = makeStyles((theme) => ({
 	btn: {
@@ -32,234 +27,193 @@ const options = {
 };
 
 function Home() {
-	const [users, setUsers] = useState([]);
-	const [nickName, setNickName] = useState("");
-	const [lastName, setLastName] = useState("");
-	const [editNickName, setEditNickName] = useState("");
-	const [editLastName, setEditLastName] = useState("");
-	const [openModalEdit, setOpenModalEdit] = useState(false);
-	const [idUserEdit, setIdUserEdit] = useState(-1);
+	const d = new Date();
+  const listUsers = [
+    {
+      id: 1,
+      profesional: "Ricardo Suarez",
+      fecha: d.getDay() + "/" + d.getMonth() + "/" + d.getFullYear(),
+      observaciones: "tiene una dentadura derecha y nonecesita brackets",
+      tipo_profesional: "Odontologo",
+    },
+  ];
+  const [users, setUsers] = useState(listUsers);
+  const [profesional, setProfesional] = useState("");
+  const [observaciones, setObservaciones] = useState("");
+  const [tipo_profesional, setTipo_profesional] = useState("");
 
-	const columns = [
-		{
-			name: "id",
-			label: "id de usuario",
-			options: {
-				filter: true,
-				sort: true,
-			},
-		},
-		{
-			name: "nickName",
-			label: "nombre",
-			options: {
-				display: true,
-			},
-		},
-		{
-			name: "lastName",
-			label: "apellido",
-			options: {
-				display: true,
-			},
-		},
-		{
-			name: "eliminar",
-			label: "Eliminar",
-			options: {
-				filter: true,
-				sort: false,
-				customBodyRender: (value, tableMeta, updateValue) => {
-					return (
-						<Button
-							variant="contained"
-							style={{ backgroundColor: "red" }}
-							onClick={() => deleteUser(tableMeta)}
-						>
-							Eliminar
-						</Button>
-					);
-				},
-			},
-		},
+  const columns = [
+    {
+      name: "id",
+      label: "id",
+      options: {
+        filter: true,
+        sort: true,
+      },
+    },
+    {
+      name: "profesional",
+      label: "profesional",
+      options: {
+        display: true,
+      },
+    },
+    {
+      name: "fecha",
+      label: "fecha",
+      options: {
+        display: true,
+      },
+    },
+    {
+      name: "observaciones",
+      label: "observaciones",
+      options: {
+        display: true,
+      },
+    },
+    {
+      name: "tipo_profesional",
+      label: "tipo de profesional",
+      options: {
+        display: true,
+      },
+    },
+    {
+      name: "eliminar",
+      label: "Eliminar",
+      options: {
+        filter: true,
+        sort: false,
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return (
+            <Button
+              variant="contained"
+              style={{ backgroundColor: "red" }}
+              onClick={() => deleteUser(tableMeta)}
+            >
+              Eliminar
+            </Button>
+          );
+        },
+      },
+    },
+  ];
 
-		{
-			name: "editar",
-			label: "Editar",
-			options: {
-				filter: true,
-				sort: false,
-				customBodyRender: (value, tableMeta, updateValue) => {
-					return (
-						<Button
-							variant="contained"
-							style={{ backgroundColor: "green" }}
-							onClick={() => updateUser(tableMeta)}
-						>
-							Actualizar
-						</Button>
-					);
-				},
-			},
-		},
-	];
 
-	useEffect(async () => {
-		const response = await getUsers();
-		setUsers(response);
-	}, []);
+  const handleObservaciones = (e) => {
+    setObservaciones(e.target.value);
+  };
 
-	const handleLastName = (e) => {
-		setLastName(e.target.value);
-	};
+  const handleProfesional = (e) => {
+    setProfesional(e.target.value);
+  };
 
-	const handleNickName = (e) => {
-		setNickName(e.target.value);
-	};
+  const handleTipo_profesional = (e) => {
+    setTipo_profesional(e.target.value);
+  };
 
-	const handleEditNickName = (e) => {
-		setEditNickName(e.target.value);
-	};
+  const deleteUser = async (tableMeta) => {
+      users.splice(tableMeta.rowIndex, 1)
+	  setUsers(users)
+	  console.log(users)
+	  setProfesional({})
 
-	const handleEditLastName = (e) => {
-		setEditLastName(e.target.value);
-	};
+		  alert("eliminado correctamente (escribe algo en el formulario para ver el cambio)")
+		  setProfesional("")
+		  setTipo_profesional("")
+		  setObservaciones("")		  
 
-	const deleteUser = async (tableMeta) => {
+  };
+
+  const addUser = () => {
+    if (profesional == "" || observaciones == "" || tipo_profesional == "") {
+      alert("debe llenar todos los campos");
+    } else {
+		if(users.length == 0){
+		var id = 1
+		}else{
+			var id = (users[users.length - 1]['id']) + 1
+		}var b = users.length	
 		let body = {
-			id: tableMeta.rowData[0],
+		  id: id,
+		  profesional: profesional,
+		  fecha: d.getDay() + "/" + d.getMonth() + "/" + d.getFullYear(),
+		  observaciones: observaciones,
+		  tipo_profesional: tipo_profesional,
 		};
-		const response = await DeleteUser(body);
-		if (response == "borrado correctamente") {
-			debugger;
-			const response2 = await getUsers();
-
-			setUsers(response2);
+		users.push(body)	
+		setUsers(users)
+		console.log(users)
+		if (users.length > b){
+			alert("agregado correctamente")
+			setProfesional({})
+			setProfesional("")
+			setTipo_profesional("")
+			setObservaciones("")		  
 		}
+	}
 	};
-
-	const editUser = async () => {
-		let body = {
-			id: idUserEdit,
-			nickName: editNickName,
-			lastName: editLastName,
-		};
-		const response = await UpdateUser(body);
-	};
-
-	const updateUser = (tableMeta) => {
-		setOpenModalEdit(true);
-
-		setIdUserEdit(tableMeta.rowData[0]);
-		setEditNickName(tableMeta.rowData[1]);
-		setEditLastName(tableMeta.rowData[2]);
-	};
-
-	const addUser = async () => {
-		if (nickName == "" || lastName == "") {
-			alert("debe llenar todos los campos");
-		} else {
-			let body = {
-				nickName: nickName,
-				lastName: lastName,
-			};
-			const response = await AddUser(body);
-			debugger;
-			if (response == "añadido correctamente") {
-				const response2 = await getUsers();
-				setLastName("");
-				setNickName("");
-				setUsers(response2);
-			} else {
-				alert("hubo un error");
-			}
-		}
-	};
-
+  
 	const classes = useStyles();
 	return (
-		<div>
-			<ThemeProvider theme={theme}>
-				<Grid container spacing={2} style={{ margin: "10% 5% 0% 5%" }}>
-					<Grid item spacing={2} container xs={3}>
-						<Grid item xs={12} className={classes.GridTextField}>
-							<TextField
-								fullWidth
-								onChange={handleNickName}
-								value={nickName}
-								label="nombre de usuario"
-							/>
-						</Grid>
-						<Grid item xs={12} className={classes.GridTextField}>
-							<TextField
-								fullWidth
-								onChange={handleLastName}
-								value={lastName}
-								label="apellido"
-							/>
-						</Grid>
-						<Grid item fullWidth xs={12} className={classes.GridTextField}>
-							<Button
-								fullWidth
-								variant="contained"
-								onClick={addUser}
-								className={classes.btn}
-							>
-								agregar
-							</Button>
-						</Grid>
-					</Grid>
-					<Grid item xs={8} style={{ width: "70%" }}>
-						<MUIDataTable
-							title={"Usuarios"}
-							data={users}
-							columns={columns}
-							options={options}
-						/>
-					</Grid>
-				</Grid>
-				<Dialog open={openModalEdit} title={"Editar Usuario"}>
-					<DialogContent>
-						<Grid container spacing={2}>
-							<Grid item xs={12} md={6}>
-								<TextField
-									value={editNickName}
-									onChange={handleEditNickName}
-									required
-									id="name"
-									label="Nombre"
-									type="text"
-									fullWidth
-								/>
-							</Grid>
-							<Grid item xs={12} md={6}>
-								<TextField
-									value={editLastName}
-									onChange={handleEditLastName}
-									required
-									id="name"
-									label="Apellido"
-									type="text"
-									fullWidth
-								/>
-							</Grid>
-
-							<Grid item xs={12} md={12}>
-								<Button
-									variant="contained"
-									color="primary"
-									onClick={editUser}
-									style={{ display: "flex", margin: "auto" }}
-								>
-									Guardar
-								</Button>
-							</Grid>
-						</Grid>
-					</DialogContent>
-				</Dialog>
-			</ThemeProvider>
-		</div>
+	  <div>
+		<ThemeProvider theme={theme}>
+		  <Typography style={{ margin: "3% 50% 0%", fontSize: "2em" }}>
+			Controles
+		  </Typography>
+		  <Grid container spacing={2} style={{ margin: "10% 5% 0% 5%" }}>
+			<Grid item spacing={2} container xs={3}>
+			  <Grid item xs={12} className={classes.GridTextField}>
+				<TextField
+				  fullWidth
+				  onChange={handleProfesional}
+				  value={profesional}
+				  label="nombre de profesional"
+				/>
+			  </Grid>
+			  <Grid item xs={12} className={classes.GridTextField}>
+				<TextareaAutosize
+				  aria-label="minimum height"
+				  minRows={2}
+				  placeholder="observaciones"
+				  variant="outlined"
+				  style={{ width: 200 }}
+				  onChange={handleObservaciones}
+				  value={observaciones}
+				/>
+			  </Grid>
+			  <Grid item xs={12} className={classes.GridTextField}>
+				<TextField
+				  fullWidth
+				  onChange={handleTipo_profesional}
+				  value={tipo_profesional}
+				  label="tipo de profesional"
+				/>
+			  </Grid>
+			  <Grid item fullWidth xs={12} className={classes.GridTextField}>
+				<Button
+				  fullWidth
+				  variant="contained"
+				  onClick={() => addUser()}
+				  className={classes.btn}
+				>
+				  agregar
+				</Button>
+			  </Grid>
+			</Grid>
+			<Grid item xs={8} style={{ width: "70%" }}>
+			  <MUIDataTable
+				title={"Usuarios"}
+				data={users}
+				columns={columns}
+				options={options}
+			  />
+			</Grid>
+		  </Grid>
+		</ThemeProvider>
+	  </div>
 	);
-}
-
-export default Home;
+  }
+  
+  export default Home;
